b2586f02005854af9530adc78ae32244e17d64e4
vntero <shin.antero@gmail.com>
2021-12-09 16:06:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import routes from "./config/routes";
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
